f4d49eb4645ba70b6b79286e257c9643af03e990
username081581 <post@thomas-hahn.org>
2021-11-16 16:51:51 +0100

    kyu6.FindTheOddInt

---
+++ csharp-dojo/Codewars/kyu6/FindTheOddInt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.kyu6
+{
+    public class FindTheOddInt
+    {
+
+        /// <summary>
+        /// Kata: 54da5a58ea159efa38000836
+        /// </summary>
+        /// <param name="seq"></param>
+        /// <returns></returns>
+        public static int find_it(int[] seq)
+        {
+            var result = seq.GroupBy(x => x)
+                .Select(group => new
+                {
+                    key = group.Key,
+                    cnt = group.Count()
+                })
+                .Where(x => x.cnt % 2 == 1).First();
+
+            return result.key;
+        }
+    }
+}
---
+++ csharp-dojo/CodewarsTests/kyu6/FindTheOddIntTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codewars.kyu6;
+
+namespace CodewarsTests.kyu6
+{
+    [TestClass]
+    public class FindTheOddIntTests
+    {
+        [TestMethod]
+        public void Tests()
+        {
+            Assert.AreEqual(5, FindTheOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+        }
+    }
+}
