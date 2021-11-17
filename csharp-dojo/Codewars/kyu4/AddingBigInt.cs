0af2044fbbe3158464a8976824c2651c9194f589
username081581 <post@thomas-hahn.org>
2021-11-17 15:14:51 +0100

    kyu4.AddingBigNumbers

---
+++ csharp-dojo/Codewars/kyu4/AddingBigInt.cs
@@ -0,0 +1,13 @@
+﻿using System.Numerics;
+
+namespace Codewars.kyu4
+{
+    public class AddingBigInt
+    {
+
+        public static string Add(string a, string b)
+        {
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+    }
+}
---
+++ csharp-dojo/CodewarsTests/kyu4/AddingBigIntTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codewars.kyu4;
+
+namespace CodewarsTests.kyu4
+{
+    [TestClass]
+    public class AddingBigIntTests
+    {
+
+        [TestMethod]
+        public  void AddTests()
+        {
+            Assert.AreEqual("110", AddingBigInt.Add("91", "19"));
+            Assert.AreEqual("1111111111", AddingBigInt.Add("123456789", "987654322"));
+            Assert.AreEqual("1000000000", AddingBigInt.Add("999999999", "1"));
+        }
+    }
+}
