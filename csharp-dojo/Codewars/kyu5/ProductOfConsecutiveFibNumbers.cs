0cf311ca23f7ab1238b7a6428d2b286bb1a643da
username081581 <post@thomas-hahn.org>
2021-11-17 15:38:34 +0100

    kyu5.ProductOfConsecutiveFibNumbers

---
+++ csharp-dojo/Codewars/kyu5/ProductOfConsecutiveFibNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.kyu5
+{
+    public class ProductOfConsecutiveFibNumbers
+    {
+
+        public static ulong[] productFib(ulong prod)
+        {
+            ulong n=1, n1=0, next;
+            while (true)
+            {
+                if ((n * n1) >= prod)
+                {
+                    return new ulong[3] { n1, n, ((n * n1) == prod) ? (ulong)1 : (ulong)0 };
+                }
+                else
+                {
+                    next = n + n1;
+                    n1 = n;
+                    n = next;
+                }
+            }
+        }
+
+    }
+}
---
+++ csharp-dojo/CodewarsTests/kyu5/ProductOfConsecutiveFibNumbersTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codewars.kyu5;
+
+namespace CodewarsTests.kyu5
+{
+
+    [TestClass]
+    public class ProductOfConsecutiveFibNumbersTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            ulong[] r = new ulong[] { 55, 89, 1 };
+            CollectionAssert.AreEqual(r, ProductOfConsecutiveFibNumbers.productFib(4895));
+        }
+    }
+}
