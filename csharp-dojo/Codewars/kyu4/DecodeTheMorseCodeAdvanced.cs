097654b9c852c83443431b7fc343a42b7d54af5e
username081581 <post@thomas-hahn.org>
2022-02-06 14:17:40 +0100

    DecodeTheMorseCodeAdvanced added classes

---
+++ csharp-dojo/Codewars/kyu4/DecodeTheMorseCodeAdvanced.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars.kyu4
+{
+    public class DecodeTheMorseCodeAdvanced
+    {
+
+        static Dictionary<string,string> _morseCode = new Dictionary<string, string>()
+                                   {
+                                       {"a", ".-"},
+                                       {"b", "-..."},
+                                       {"c", "-.-."},
+                                       {"d", "-.."},
+                                       {"e", "."},
+                                       {"f", "..-."},
+                                       {"g", "--."},
+                                       {"h", "...."},
+                                       {"i", ".."},
+                                       {"j", ".---"},
+                                       {"k", "-.-"},
+                                       {"l", ".-.."},
+                                       {"m", "--"},
+                                       {"n", "-."},
+                                       {"o", "---"},
+                                       {"p", ".--."},
+                                       {"q", "--.-"},
+                                       {"r", ".-."},
+                                       {"s", "..."},
+                                       {"t", "-"},
+                                       {"u", "..-"},
+                                       {"v", "...-"},
+                                       {"w", ".--"},
+                                       {"x", "-..-"},
+                                       {"y", "-.--"},
+                                       {"z", "--.."},
+                                       {"0", "-----"},
+                                       {"1", ".----"},
+                                       {"2", "..---"},
+                                       {"3", "...--"},
+                                       {"4", "....-"},
+                                       {"5", "....."},
+                                       {"6", "-...."},
+                                       {"7", "--..."},
+                                       {"8", "---.."},
+                                       {"9", "----."}
+                                   };
+
+        public static string DecodeBits(string bits)
+        {
+            
+            return ".";
+        }
+
+        public static string DecodeMorse(string morseCode)
+        {
+            return MorseCode.Get(morseCode);
+        }
+
+        private class MorseCode
+        {
+            public static string Get(string MorseCode)
+            {
+                if (_morseCode.ContainsKey(MorseCode))
+                {
+                    return _morseCode[MorseCode];
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+    }
+}
---
+++ csharp-dojo/CodewarsTests/kyu4/DecodeTheMorseCodeAdvancedTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodewarsTests.kyu4
+{
+    [TestClass]
+    public class DecodeTheMorseCodeAdvancedTests
+    {
+
+        [TestMethod]
+        public void TestFromDescription()
+        {
+            Assert.AreEqual("HEY JUDE", DecodeTheMorseCodeAdvanced.DecodeMorse(DecodeTheMorseCodeAdvanced.DecodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")));
+        }
+    }
+}
