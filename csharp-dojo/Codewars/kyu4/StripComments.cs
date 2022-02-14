adc53406d37a6aa6b454ff8ca79815aa5fefb23b
hahn-th <post@thomas-hahn.org>
2022-02-14 23:10:19 +0100

    kyu4.StripComments

---
+++ csharp-dojo/Codewars/kyu4/StripComments.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Codewars.kyu4
+{
+    /*
+     * 
+     * https://www.codewars.com/kata/51c8e37cee245da6b40000bd
+     * 
+     * 
+     * Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+        Example:
+
+        Given an input string of:
+
+        apples, pears # and bananas
+        grapes
+        bananas !apples
+
+        The output expected would be:
+
+        apples, pears
+        grapes
+        bananas
+
+        The code would be called like so:
+
+        string stripped = StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new [] { "#", "!" })
+        // result should == "apples, pears\ngrapes\nbananas"
+
+    */
+    public class StripCommentsSolution
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            Regex r = new Regex(@"([ ]?[" + string.Join("", commentSymbols) + @"].*|[ ]+)$", RegexOptions.Multiline | RegexOptions.IgnoreCase);
+            return r.Replace(text, "");
+        }
+    }
+}
---
+++ csharp-dojo/CodewarsTests/kyu4/StripCommentsTests.cs
@@ -0,0 +1,40 @@
+﻿using Codewars.kyu4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsTests.kyu4
+{
+    [TestClass]
+    public class StripCommentsTests
+    {
+        [TestMethod]
+        public void StripComments()
+        {
+            Assert.AreEqual(
+                    "apples, pears\ngrapes\nbananas",
+                    StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+
+            Assert.AreEqual("a\nc\nd", StripCommentsSolution.StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+
+            Assert.AreEqual("\n\nFB\n\nA\n\nD\n\nFF", StripCommentsSolution.StripComments("--\n\nFB\n\nA\n\nD\n\nFF-B", new string[] { "#", "$", "!", "-" }));
+            Assert.AreEqual("a\n b\nc", StripCommentsSolution.StripComments("a \n b \nc ", new string[] { "#", "$" }));
+        }
+
+        [TestMethod]
+        public void StripComments2()
+        {
+            Assert.AreEqual("a\n b\nc", StripCommentsSolution.StripComments("a \n b \nc ", new string[] { "#", "$" }));
+            Assert.AreEqual(String.Empty, StripCommentsSolution.StripComments("a", new string[] { "a" }));
+        }
+
+        [TestMethod]
+        public void StripComments3()
+        {
+            Assert.AreEqual("\n\nAB\nd", StripCommentsSolution.StripComments("\n\nAB-\nd", new string[] { "-" }));
+        }
+    }
+}
